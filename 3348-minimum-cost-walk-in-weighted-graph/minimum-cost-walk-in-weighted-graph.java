2ce04573231dd2c47eb3c26155050b7aed61c3a0
Pawan Kumar <119093847+Pawan-kumar47129@users.noreply.github.com>
2025-03-30 16:40:17 +0530

    Time: 28 ms (14.01%) | Memory: 95.7 MB (99.59%) - LeetSync

--- 3348-minimum-cost-walk-in-weighted-graph/minimum-cost-walk-in-weighted-graph.java
+++ 3348-minimum-cost-walk-in-weighted-graph/minimum-cost-walk-in-weighted-graph.java
@@ -1,3 +1,5 @@
+//Using DisJointSet TC=(N+E+Q)
+/*
 class DisJointSet{
     int []parent,size;
     public DisJointSet(int n){
@@ -55,6 +57,67 @@ class Solution {
                 int root=dsu.find(start);
                 //return the precomputed cost of the component
                 ans[i]=componentCost[root];
+            }
+        }
+        return ans;
+    }
+}*/
+
+class Solution {
+    private int getComponentCost(int src,List<List<int[]>>adjList,int[]component,int componentId,boolean[]visited){
+        Queue<Integer>queue=new LinkedList<>();
+        queue.add(src);
+        visited[src]=true;
+        int componentCost=Integer.MAX_VALUE;
+        while(!queue.isEmpty()){
+            int node=queue.poll();
+            //mark the node as part of the current component
+            component[node]=componentId;
+            // Explore all neighbors of the current node
+            for(int[]neighbor:adjList.get(node)){
+                int weight=neighbor[1];
+                 // Update the component cost by performing a bitwise AND of the edge weights
+                componentCost &=weight;
+                // If the neighbor hasn't been visited, mark it as visited and add it to the queue
+                if(visited[neighbor[0]]) continue;
+                queue.add(neighbor[0]);
+                visited[neighbor[0]]=true;
+            }
+        }
+        return componentCost;
+    }
+    public int[] minimumCost(int n, int[][] edges, int[][] query) {
+        //make adjList
+        List<List<int[]>>adjList=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            adjList.add(new ArrayList<>());
+        }
+        for(int[]edge:edges){
+            adjList.get(edge[0]).add(new int[]{edge[1],edge[2]});
+            adjList.get(edge[1]).add(new int[]{edge[0],edge[2]});
+        }
+        boolean[]visited=new boolean[n];
+        //Array to store componentId for each node
+        int[]component=new int[n];
+        int[]componentCost=new int[n];
+        int componentId=0;
+        //perform bfs for each unvisited node to indentify components and calculate their cost
+        for(int node=0;node<n;node++){
+            if(!visited[node]){
+                componentCost[componentId]=getComponentCost(node,adjList,component,componentId,visited);
+                componentId++;
+            }
+        }
+
+        //gives ans of each query
+        int[]ans=new int[query.length];
+        for(int i=0;i<query.length;i++){
+            int start=query[i][0];
+            int end=query[i][1];
+            if(component[start]!=component[end]){//if both node are not in same componentId
+                ans[i]=-1;
+            }else{
+                ans[i]=componentCost[component[start]];//find cost of conponent[start]id
             }
         }
         return ans;
