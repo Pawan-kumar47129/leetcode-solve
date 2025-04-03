3389f1d6f491517ca7a97b54e1c6b700fd47d057
Pawan Kumar <119093847+Pawan-kumar47129@users.noreply.github.com>
2025-04-03 10:36:35 +0530

    Time: 16 ms (86.79%) | Memory: 45.5 MB (12.53%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
@@ -1,5 +1,5 @@
-class Solution {
-    
+//Memoization TC=O(n*m) Sc=O(n*m)+O(n+m)
+/*class Solution {
     long solve(int i,int j,String s1,String s2,Long[][]dp){
         if(i<=0 || j<=0) return 0;
         if(dp[i][j]!=null) return dp[i][j];
@@ -22,9 +22,32 @@ class Solution {
             allCharASCII+=s2.charAt(i);
         }
 
-        return (int)(allCharASCII-lcsSum*2);
+        return (int)(allCharASCII-lcsSum*2);  
+    }
+}*/
 
-        
-
+class Solution {
+    public int minimumDeleteSum(String s1, String s2) {
+        int n=s1.length();
+        int m=s2.length();
+        long[][]dp=new long[n+1][m+1];
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1]+s1.charAt(i-1);
+                }else{
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        long lcsSum=dp[n][m];
+        long allCharASCII=0;
+        for(int i=0;i<n;i++){
+            allCharASCII+=s1.charAt(i);
+        }
+        for(int i=0;i<m;i++){
+            allCharASCII+=s2.charAt(i);
+        }
+        return (int)(allCharASCII-lcsSum*2);  
     }
 }
