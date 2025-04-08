83da006e2dfa10c1e1aed9db64e963dd0d437a24
Pawan Kumar <119093847+Pawan-kumar47129@users.noreply.github.com>
2025-04-08 11:19:32 +0530

    Time: 1 ms (98.39%) | Memory: 44.5 MB (41.29%) - LeetSync

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.java
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.java
@@ -1,3 +1,5 @@
+//using HashMap TC=O(2n) Sc=O(n)
+/*
 class Solution {
     public int minimumOperations(int[] nums) {
         HashMap<Integer,Integer>map=new HashMap<>();
@@ -20,6 +22,35 @@ class Solution {
             }else{
                 right=left;
                 if(right<n) map.put(nums[right],1);
+                right++;
+            }
+        }
+        return ans;
+    }
+}*/
+
+//using normal array
+class Solution {
+    public int minimumOperations(int[] nums) {
+        int []map=new int[101];
+        int left=0;
+        int right=0;
+        int n=nums.length;
+        int ans=0;
+        while(right<n){
+            while(map[nums[right]]==1){
+                ans++;
+                if(left<n)map[nums[left]]=0;
+                if(left+1<n) map[nums[left+1]]=0;
+                if(left+2<n) map[nums[left+2]]=0;
+                left+=3;
+            }
+            if(left<=right){
+                map[nums[right]]=1;
+                right++;
+            }else{
+                right=left;
+                if(right<n) map[nums[right]]=1;
                 right++;
             }
         }
