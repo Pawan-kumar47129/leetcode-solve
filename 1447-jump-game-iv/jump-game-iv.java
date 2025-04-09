a6b3799b771464010918ae73c7cae5ec0277073e
Pawan Kumar <119093847+Pawan-kumar47129@users.noreply.github.com>
2025-04-09 22:31:39 +0530

    Time: 64 ms (42.64%) | Memory: 61.9 MB (48.57%) - LeetSync

---
+++ 1447-jump-game-iv/jump-game-iv.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int minJumps(int[] nums) {
+        int n=nums.length;
+        HashMap<Integer,List<Integer>>indexMap=new HashMap<>();
+        for(int i=0;i<n;i++){
+            indexMap.putIfAbsent(nums[i],new ArrayList<>());
+            indexMap.get(nums[i]).add(i);
+        }
+        boolean[]visited=new boolean[n];
+        Queue<Integer>queue=new LinkedList<>();
+        queue.add(0);
+        visited[0]=true;
+        int steps=0;
+        while(!queue.isEmpty()){
+            int size=queue.size();
+            for(int i=0;i<size;i++){
+                int index=queue.poll();
+                if(index==n-1) return steps;
+                if(index-1>=0 && !visited[index-1]){
+                    queue.add(index-1);
+                    visited[index-1]=true;
+                }
+                if(index+1<n && !visited[index+1]){
+                    queue.add(index+1);
+                }
+                List<Integer>list=indexMap.get(nums[index]);
+                if(list==null) continue;
+                for(var ele:list){
+                    if(!visited[ele]){
+                        queue.add(ele);
+                        visited[ele]=true;
+                    }
+                }
+                indexMap.remove(nums[index]);
+            }
+            steps++;
+        }
+        return steps;
+    }
+}
