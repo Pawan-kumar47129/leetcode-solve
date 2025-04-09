8e3ff1acd699a1e6b10b8768b39c9bd4ad2ae9b1
Pawan Kumar <119093847+Pawan-kumar47129@users.noreply.github.com>
2025-04-09 09:55:43 +0530

    Time: 3 ms (52.54%) | Memory: 44.6 MB (33.22%) - LeetSync

--- 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.java
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.java
@@ -1,23 +1,13 @@
 class Solution {
-    private void reverse(int[]nums){
-        int i=0,j=nums.length-1;
-        while(i<j){
-            int temp=nums[i];
-            nums[i]=nums[j];
-            nums[j]=temp;
-            i++;j--;
-        }
-    }
     public int minOperations(int[] nums, int k) {
         int n=nums.length;
         Arrays.sort(nums);
-        reverse(nums);
         int ans=0;
-        for(int i=1;i<n;i++){
-            if(nums[i-1]!=nums[i]) ans++;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i+1]!=nums[i]) ans++;
         }
-        if(nums[n-1]<k) return -1;
-        if(nums[n-1]==k) return ans;
+        if(nums[0]<k) return -1;
+        if(nums[0]==k) return ans;
         return ans+1;//one more steps required to get k
 
 
