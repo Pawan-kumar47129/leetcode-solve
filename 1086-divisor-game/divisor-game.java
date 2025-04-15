1f75a0d1a2cc0d4471faced3bb17fe63b325142e
Pawan Kumar <119093847+Pawan-kumar47129@users.noreply.github.com>
2025-04-15 16:48:04 +0530

    Time: 2 ms (6.52%) | Memory: 41 MB (6.01%) - LeetSync

--- 1086-divisor-game/divisor-game.java
+++ 1086-divisor-game/divisor-game.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     private boolean solve(int n, int turn, Boolean[][] dp) {
         // Base case: if only 1 is left
@@ -43,4 +44,52 @@ class Solution {
         Boolean[][] dp = new Boolean[n + 1][2];  // dp[n][turn] => result of game for current n and turn
         return solve(n, 0, dp);  // 0 = Alice's turn, 1 = Bob's turn
     }
+}*/
+
+class Solution {
+    public boolean divisorGame(int num) {
+        boolean[][] dp = new boolean[num + 1][2];  // dp[n][turn] => result of game for current n and turn
+        dp[1][0]=false;
+        dp[1][1]=true;
+        for(int n=2;n<=num;n++){
+            for(int turn=0;turn<2;turn++){
+                if (turn == 0) {
+                    // Alice's turn - she tries to force a win
+                    boolean win = dp[n - 1][1];
+                    if(win){
+                        dp[n][turn]=win;
+                        continue;
+                    }
+                    for (int i = 2; i * i <= n; i++) {
+                        if (n % i == 0) {
+                            // Try both n - i and n - (n / i)
+                            win = win || dp[n - i][1] || dp[n - n / i][1];
+                            if (win)break;
+                        }
+                    }
+                    dp[n][turn] = win;
+
+                } else {
+                    // Bob's turn - he tries to force a win (means Alice loses)
+                    boolean win = dp[n - 1][0];
+                    if (!win) {
+                        dp[n][turn] = false;
+                        continue;
+                    }
+
+                    for (int i = 2; i * i <= n; i++) {
+                        if (n % i == 0) {
+                            // Bob wins if any move forces Alice into a losing position
+                            win = dp[n - i][0] && dp[n - n / i][0];
+                            if (!win) break;
+                        }
+                    }
+
+                    dp[n][turn] = win;
+                }
+            }
+        }
+        return dp[num][0]; 
+    }
 }
+
