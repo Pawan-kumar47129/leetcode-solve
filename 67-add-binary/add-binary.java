dac3b2347f2ae0341e5af74f643ea4fe7b73845d
Pawan Kumar <119093847+Pawan-kumar47129@users.noreply.github.com>
2025-04-30 07:09:49 +0530

    Time: 1 ms (99.55%) | Memory: 42.2 MB (81.75%) - LeetSync

--- 67-add-binary/add-binary.java
+++ 67-add-binary/add-binary.java
@@ -1,65 +1,18 @@
 class Solution {
     public String addBinary(String a, String b) {
-        int n = a.length() - 1;
-        int m = b.length() - 1;
-        int carry = 0;
-        StringBuilder ans = new StringBuilder();
-        while (n >= 0 && m >= 0) {
-            char ch1 = a.charAt(n);
-            char ch2 = b.charAt(m);
-            int val = (int) ((ch1 - '0') + (ch2 - '0'));
-            //int val=(int)(ch)-'0';
-            val += carry;
-            if (val == 0) {
-                ans.append('0');
-                carry = 0;
-            } else if (val == 1) {
-                ans.append('1');
-                carry = 0;
-            } else if (val == 2) {
-                ans.append('0');
-                carry = 1;
-            } else {
-                ans.append('1');
-                carry = 1;
+        int i=a.length()-1;
+        int j=b.length()-1;
+        int carry=0;
+        StringBuilder ans=new StringBuilder();
+        while(i>=0 || j>=0 || carry==1){
+            if(i>=0){
+                carry+=a.charAt(i--) - '0';
             }
-            n--;
-            m--;
-        }
-        while (n >= 0) {
-            char ch = a.charAt(n);
-            int val = (int) (ch - '0');
-            val += carry;
-            if (val == 0) {
-                ans.append('0');
-                carry = 0;
-            } else if (val == 1) {
-                ans.append('1');
-                carry = 0;
-            } else {
-                ans.append('0');
-                carry = 1;
+            if(j>=0){
+                carry+=b.charAt(j--) - '0';
             }
-            n--;
-        }
-        while (m >= 0) {
-            char ch = b.charAt(m);
-            int val = (int) (ch - '0');
-            val += carry;
-            if (val == 0) {
-                ans.append('0');
-                carry = 0;
-            } else if (val == 1) {
-                ans.append('1');
-                carry = 0;
-            } else {
-                ans.append('0');
-                carry = 1;
-            }
-            m--;
-        }
-        if (carry == 1) {
-            ans.append('1');
+            ans.append(carry%2);
+            carry=carry/2;
         }
         return ans.reverse().toString();
     }
