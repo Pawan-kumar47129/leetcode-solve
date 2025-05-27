f2c5f11cea56de28067e104e4411b0f036b87696
Pawan Kumar <119093847+Pawan-kumar47129@users.noreply.github.com>
2025-05-27 16:47:30 +0530

    Time: 5 ms (1.30%) | Memory: 40.7 MB (68.69%) - LeetSync

---
+++ 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int differenceOfSums(int n, int m) {
+        int div=n/m;
+        long totalSum=n*(n+1)/2L;
+        long divisibleSum=div*(2*m + (div-1)*m)/2;
+        System.out.println(divisibleSum);
+        long notDivisibleSum=totalSum-divisibleSum;
+        return (int)(notDivisibleSum-divisibleSum);
+    }
+}
