92c0ac96e19822f24347df29650b9fcd26ff61f6
Pawan Kumar <119093847+Pawan-kumar47129@users.noreply.github.com>
2025-05-30 09:40:04 +0530

    Time: 19 ms (43.82%) | Memory: 62.2 MB (58.66%) - LeetSync

---
+++ 2438-find-closest-node-to-given-two-nodes/find-closest-node-to-given-two-nodes.java
@@ -0,0 +1,36 @@
+class Solution {
+    private int[]bfs(int []edges,int src){
+        int n=edges.length;
+        Queue<Integer>queue=new LinkedList<>();
+        int []dist=new int[n];
+        Arrays.fill(dist,(int)1e6);
+        dist[src]=0;
+        queue.add(src);
+        int steps=0;
+        while(!queue.isEmpty()){
+            int node=queue.poll();
+            int nextDist=dist[node]+1;
+            if(edges[node]!=-1 && dist[edges[node]]>nextDist){
+                queue.add(edges[node]);
+                dist[edges[node]]=nextDist;
+            }
+        }
+        return dist;
+    }
+    public int closestMeetingNode(int[] edges, int node1, int node2) {
+        int n=edges.length;
+        int dist1[]=bfs(edges,node1);
+        int dist2[]=bfs(edges,node2);
+        int ans=n+1;
+        int minDist=(int)1e6;
+        for(int i=0;i<n;i++){
+            int distance=Math.max(dist1[i],dist2[i]);
+            if(minDist>distance){
+                minDist=distance;
+                ans=i;
+            }
+        }
+        if(minDist==1e6) return -1;
+        return ans;
+    }
+}
