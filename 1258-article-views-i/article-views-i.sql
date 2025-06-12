99b5f4ce580d0bc2b1853dd7e43f0666476688de
Pawan Kumar <119093847+Pawan-kumar47129@users.noreply.github.com>
2025-06-12 23:31:04 +0530

    Time: 404 ms (20.17%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,2 +1,5 @@
-# Write your MySQL query statement below
-SELECT author_id  AS id FROM views WHERE author_id  = viewer_id GROUP BY author_id ORDER BY author_id ASC ;
+-- Write your PostgreSQL query statement below
+SELECT DISTINCT author_id as id
+FROM views
+WHERE author_id=viewer_id
+ORDER BY id;
