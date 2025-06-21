799518d576a2c1fe853f367c72d988ad01119be3
Pawan Kumar <119093847+Pawan-kumar47129@users.noreply.github.com>
2025-06-21 12:36:28 +0530

    Time: 2 ms (66.67%) | Memory: 41.7 MB (98.56%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.java
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     private boolean func(int i,int j,String s,String p,Boolean[][]dp){
         if(i==0 && j==0) return true;
         if(j==0) return false;
@@ -25,5 +25,34 @@ class Solution {
         int m=p.length();
         Boolean[][]dp=new Boolean[n+1][m+1];
         return func(n,m,s,p,dp);
+    }
+}*/
+
+//tabulation
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int n=s.length();
+        int m=p.length();
+        boolean[][]dp=new boolean[n+1][m+1];
+        dp[0][0]=true;
+        for (int j = 2; j <= m; j++) {
+            if (p.charAt(j - 1) == '*' && dp[0][j - 2]) {
+                dp[0][j] = true;
+            }
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s.charAt(i-1)==p.charAt(j-1) || p.charAt(j-1)=='.'){
+                    dp[i][j]=dp[i-1][j-1];
+                }else if(p.charAt(j-1)=='*'){
+                    if(s.charAt(i-1)==p.charAt(j-2) || p.charAt(j-2)=='.'){
+                        dp[i][j]=dp[i][j-2]|| dp[i-1][j];//completely skill two character or matching with p.charAt(j-2) multiple time
+                    }else{
+                        dp[i][j]=dp[i][j-2];//not matching then skip the both any* any can be . or character
+                    }
+                }else dp[i][j]=false;
+            }
+        }
+        return dp[n][m];
     }
 }
