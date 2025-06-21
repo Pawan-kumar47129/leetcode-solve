5069a172cf83394be13bfef162da226e5f9c35b4
Pawan Kumar <119093847+Pawan-kumar47129@users.noreply.github.com>
2025-06-21 10:21:29 +0530

    Time: 1 ms (100.00%) | Memory: 42.3 MB (49.10%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.java
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -1,5 +1,5 @@
 class Solution {
-    private boolean func(int i,int j,String s,String p){
+    private boolean func(int i,int j,String s,String p,Boolean[][]dp){
         if(i==0 && j==0) return true;
         if(j==0) return false;
         if(i==0){
@@ -7,21 +7,23 @@ class Solution {
                 if(p.charAt(j-1)!='*') return false;
                 j-=2;
             }
-            return true;
+            return dp[i][j]= true;
         }
+        if(dp[i][j]!=null) return dp[i][j];
         if(s.charAt(i-1)==p.charAt(j-1) || p.charAt(j-1)=='.'){
-            return func(i-1,j-1,s,p);
+            return dp[i][j]=func(i-1,j-1,s,p,dp);
         }else if(p.charAt(j-1) == '*'){
                 if(s.charAt(i-1)==p.charAt(j-2) || p.charAt(j-2)=='.'){
-                    return func(i,j-2,s,p) || func(i-1,j,s,p);
+                    return dp[i][j]= func(i,j-2,s,p,dp) || func(i-1,j,s,p,dp);//completely skill two character or matching with p.charAt(j-2) multiple time
                 }
-                else  return func(i,j-2,s,p);
+                else  return dp[i][j]= func(i,j-2,s,p,dp);//not matching then skip the both any* any can be . or character
         }
-        else return false;
+        else return dp[i][j]=false;
     }
     public boolean isMatch(String s, String p) {
         int n=s.length();
         int m=p.length();
-        return func(n,m,s,p);
+        Boolean[][]dp=new Boolean[n+1][m+1];
+        return func(n,m,s,p,dp);
     }
 }
