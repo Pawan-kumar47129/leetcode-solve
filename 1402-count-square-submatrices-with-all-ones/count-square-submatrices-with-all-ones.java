1826da6473ca21d329a224a2fdf32eaa9f776e92
Pawan Kumar <119093847+Pawan-kumar47129@users.noreply.github.com>
2025-08-20 23:26:14 +0530

    Time: 5 ms (98.48%) | Memory: 55.2 MB (44.80%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     int n,m;
     private int solve(int i,int j,int[][]matrix,Integer[][]dp){
         if(i>=n || j>=m || matrix[i][j]==0) return 0;
@@ -18,6 +18,31 @@ class Solution {
                 if(matrix[i][j]==1){
                     result+=solve(i,j,matrix,dp);
                 }
+            }
+        }
+        return result;
+    }
+}*/
+
+class Solution {
+    public int countSquares(int[][] matrix) {
+        int n,m;
+        n=matrix.length;
+        m=matrix[0].length;
+        int result=0;
+        int[][]dp=new int[n+1][m+1];
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(matrix[i][j]==0) continue;
+                int right=dp[i][j+1];
+                int dig=dp[i+1][j+1];
+                int down=dp[i+1][j];
+                dp[i][j]=1+ Math.min(right,Math.min(dig,down));
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                result+=dp[i][j];
             }
         }
         return result;
