795a47457360ac54e6e75cdb4e1a58d2b0eff849
Pawan Kumar <119093847+Pawan-kumar47129@users.noreply.github.com>
2025-09-08 00:30:51 +0530

    Time: 261 ms (75.62%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,4 +1,14 @@
--- Write your PostgreSQL query statement below
+# Write your MySQL query statement below
+
+/*
+SELECT (SELECT salary 
+FROM Employee
+GROUP BY salary
+ORDER BY salary DESC
+LIMIT 1
+OFFSET 1) as SecondHighestSalary;
+*/
+
 SELECT MAX(salary) as SecondHighestSalary
-FROM employee
-WHERE salary !=(SELECT MAX(salary) from employee);
+FROM Employee
+WHERE salary<(SELECT MAX(salary) FROM Employee);
