02faa2178fad2777a3a2897be6bd8e776b3691a9
Pawan Kumar <119093847+Pawan-kumar47129@users.noreply.github.com>
2025-09-08 00:15:49 +0530

    Time: 367 ms (93.23%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,4 +1,5 @@
--- Write your PostgreSQL query statement below
-SELECT p.firstName, p.lastName ,a.city ,a.state 
-FROM person p LEFT JOIN address a
+# Write your MySQL query statement below
+SELECT p.firstName,p.lastName,a.city,a.state 
+FROM Person as p
+LEFT JOIN Address as a 
 ON p.personId=a.personId;
