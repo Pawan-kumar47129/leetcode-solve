bc21e0b1a999d2f6cfdcb345a05cdca3dae13934
Pawan Kumar <119093847+Pawan-kumar47129@users.noreply.github.com>
2025-09-10 12:33:13 +0530

    Time: 22 ms (81.52%) | Memory: 51.4 MB (79.02%) - LeetSync

--- 3999-minimum-operations-to-transform-string/minimum-operations-to-transform-string.java
+++ 3999-minimum-operations-to-transform-string/minimum-operations-to-transform-string.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public int minOperations(String s) {
         int ans=0;
         TreeMap<Character,Integer>charMap=new TreeMap<>((a,b)->a-b);
@@ -16,6 +16,23 @@ class Solution {
                 charMap.put(nextChar,charMap.getOrDefault(nextChar,0)+freq);
                 charMap.remove(ch);
                 ans++;
+            }
+        }
+        return ans;
+    }
+}*/
+
+class Solution {
+    public int minOperations(String s) {
+        int ans=0;
+        int[]freq=new int[26];
+        for(char ch:s.toCharArray()){
+            freq[ch-'a']++;
+        }
+        for(int i=1;i<26;i++){
+            if(freq[i]!=0){
+                ans++;
+                freq[(i+1)%26]+=freq[i];
             }
         }
         return ans;
