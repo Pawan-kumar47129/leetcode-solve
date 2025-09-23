09d8b7f10adcb0b7585b6277d994f29e64ef6e9c
Pawan Kumar <119093847+Pawan-kumar47129@users.noreply.github.com>
2025-09-23 09:33:39 +0530

    Time: 340 ms (86.55%) | Memory: 0B (100.00%) - LeetSync

--- 182-duplicate-emails/duplicate-emails.sql
+++ 182-duplicate-emails/duplicate-emails.sql
@@ -1,5 +1,11 @@
 # Write your MySQL query statement below
+/*
 SELECT distinct(p1.email) as Email 
 FROM person p1
 INNER JOIN person p2 ON
 p1.email=p2.email and p1.id<>p2.id;
+*/
+SELECT email 
+FROM Person
+GROUP BY email
+having count(email)>1;
